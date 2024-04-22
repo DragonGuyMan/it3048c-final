0651be2751eee4675ac0b4a9e8e4e90a3fa83d85
Carson Simms <143337976+Carson-Simms@users.noreply.github.com>
2024-04-21 22:20:26 -0400

    Added logic to edit and copy buttons on list view

--- Views/AccountListView.xaml.cs
+++ Views/AccountListView.xaml.cs
@@ -1,28 +1,37 @@
-using IT3048C_Final.Data;
 using IT3048C_Final.Models;
+using IT3048C_Final.ViewModels;
 
 namespace IT3048C_Final.Views;
 
 public partial class AccountListView : ContentPage
 {
-	AccountDB database;
-
-	public AccountListView(AccountDB accountDB)
+	public AccountListView(AccountEntryViewModel vm)
 	{
-		InitializeComponent();
-        BindingContext = this;
-		database = accountDB;
+        InitializeComponent();
+        BindingContext = vm;
     }
-
-	protected override async void OnAppearing()
-	{
-		base.OnAppearing();
-        AccountList.ItemsSource = await database.GetAccountEntriesAsync();
-	}
-
     private async void AddAccount(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//AccountEntry");
     }
 
+    private async void CopyPassword(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.BindingContext is AccountEntry account)
+        {
+            if (!string.IsNullOrEmpty(account.Password))
+            {
+                await Clipboard.Default.SetTextAsync(account.Password);
+                await Shell.Current.DisplayAlert("Success", "Password copied to clipboard.", "OK");
+            }
+        }
+    }
+    private async void EditAccount(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.BindingContext is AccountEntry account)
+        {
+            var navigationParameter = $"?Id={account.ID}&Name={account.Name}&Username={account.Username}&Password={account.Password}";
+            await Shell.Current.GoToAsync($"//AccountEntry{navigationParameter}");
+        }
+    }
 }
