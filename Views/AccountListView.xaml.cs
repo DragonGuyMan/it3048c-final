e35b667bf99d06b0d1d06be0775b2b74567fd699
Carson Simms <143337976+Carson-Simms@users.noreply.github.com>
2024-04-21 22:27:00 -0400

    Added back logic to load list page

--- Views/AccountListView.xaml.cs
+++ Views/AccountListView.xaml.cs
@@ -1,18 +1,27 @@
+using IT3048C_Final.Data;
 using IT3048C_Final.Models;
-using IT3048C_Final.ViewModels;
 
 namespace IT3048C_Final.Views;
 
 public partial class AccountListView : ContentPage
 {
-	public AccountListView(AccountEntryViewModel vm)
-	{
+    AccountDB database;
+
+    public AccountListView(AccountDB accountDB)
+    {
         InitializeComponent();
-        BindingContext = vm;
+        BindingContext = this;
+        database = accountDB;
     }
     private async void AddAccount(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//AccountEntry");
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        AccountList.ItemsSource = await database.GetAccountEntriesAsync();
     }
 
     private async void CopyPassword(object sender, EventArgs e)
